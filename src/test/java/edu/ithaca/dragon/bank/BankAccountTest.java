cdb761d6d60a38bb10e7f9a6e52b5bc15a7a34ad
paxmeny <36705016+paxmeny@users.noreply.github.com>
2021-01-28 11:35:33 -0500

    withdrawTest tests += amount>Balance && amount < 0

--- src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
@@ -18,8 +18,9 @@ class BankAccountTest {
         BankAccount bankAccount = new BankAccount("a@b.com", 200);
         bankAccount.withdraw(100);
 
-        assertEquals(100, bankAccount.getBalance());
-        assertThrows(InsufficientFundsException.class, () -> bankAccount.withdraw(300));
+        assertEquals(100, bankAccount.getBalance()); //Makes sure a valid withdraw works
+        assertThrows(InsufficientFundsException.class, () -> bankAccount.withdraw(300)); //Amount > Balance
+        assertThrows(InsufficientFundsException.class, () -> bankAccount.withdraw(-1)); //Amount < 0
     }
 
     @Test
